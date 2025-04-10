581c30e44f91b23862e4f618acc8a74eb7e806a2
Riccardo Caravagna <riccardocaravagna@gmail.com>
2025-04-10 18:50:55 +0200

    Fix book ID in books data and add book retrieval endpoints in the router

--- app/data/books.py
+++ app/data/books.py
@@ -2,6 +2,6 @@ from models.book import Book
 
 books = {
     0: Book(id = 0, title = "Libro zero", author = "Autore zero", review = 1),
-    1: Book(id = 2, title = "Libro uno", author = "Autore uno", review = 2),
+    1: Book(id = 1, title = "Libro uno", author = "Autore uno", review = 2),
     2: Book(id = 2, title = "Libro due", author = "Autore due", review = 5),
 }
--- app/routers/books.py
+++ app/routers/books.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Path
+from typing import Annotated
 from models.book import Book
 from data.books import books
 
@@ -6,4 +7,13 @@ router = APIRouter(prefix = "/books")
 
 @router.get("/")
 def get_all_books() -> list[Book]:
+    """Returns the list of available books."""
     return list(books.values())
+
+@router.get("/{id}")
+def get_book_by_id(id: Annotated[int, Path(description = "The ID of the book to get")]) -> Book:
+    """Returns the book with the given id."""
+    try:
+        return books[id]
+    except KeyError:
+        raise HTTPException(status_code = 404, detail = "Book not found.")
