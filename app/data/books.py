d04e2a58e847142b7527bfab415337a2549026cc
Riccardo Caravagna <riccardocaravagna@gmail.com>
2025-04-11 11:34:34 +0200

    Implement book management features: add sorting, CRUD operations, and review functionality

--- app/data/books.py
+++ app/data/books.py
@@ -1,7 +1,7 @@
 from models.book import Book
 
 books = {
-    0: Book(id = 0, title = "Libro zero", author = "Autore zero", review = 1),
-    1: Book(id = 1, title = "Libro uno", author = "Autore uno", review = 2),
-    2: Book(id = 2, title = "Libro due", author = "Autore due", review = 5),
+    0: Book(id = 0, title = "Libro zero", author = "Autore zero", review = 5),
+    1: Book(id = 1, title = "Libro uno", author = "Autore uno", review = 1),
+    2: Book(id = 2, title = "Libro due", author = "Autore due", review = 3),
 }
--- app/routers/books.py
+++ app/routers/books.py
@@ -1,19 +1,73 @@
 from fastapi import APIRouter, HTTPException, Path
 from typing import Annotated
 from models.book import Book
+from models.review import Review
 from data.books import books
 
 router = APIRouter(prefix = "/books")
 
 @router.get("/")
-def get_all_books() -> list[Book]:
-    """Returns the list of available books."""
-    return list(books.values())
+def get_all_books(
+    sort: bool = False
+) -> list[Book]:
+    """Returns the list of available books"""
+    if sort:
+        return sorted(books.values(), key = lambda book: book.review)
+    else:
+        return list(books.values())
+    
+@router.post("/")
+def post_book(book: Book):
+    """Adds a new book"""
+    if book.id in books:
+        raise HTTPException(status_code = 403, detail = "Book with given ID already present")
+    books[book.id] = book
+    return "Book added successfully"
+
+@router.delete("/")
+def delete_all_books():
+    """Deletes all books"""
+    books.clear()
+    return "All books deleted successfully"
 
 @router.get("/{id}")
 def get_book_by_id(id: Annotated[int, Path(description = "The ID of the book to get")]) -> Book:
-    """Returns the book with the given id."""
+    """Returns the book with the given id"""
     try:
         return books[id]
     except KeyError:
-        raise HTTPException(status_code = 404, detail = "Book not found.")
+        raise HTTPException(status_code = 404, detail = "Book not found")
+    
+@router.put("/{id}")
+def update_book(
+    id: Annotated[int, Path(description = "The ID of the book to update")],
+    book: Book
+):
+    """Updates the book with the given ID"""
+    if not id in books:
+        raise HTTPException(status_code = 404, detail = "Book not found")
+    books[id] = book
+    return "Book updated successfully"
+
+@router.delete("/{id}")
+def delete_book(
+    id: Annotated[int, Path(description = "The ID of the book to delete")],
+):
+    """Deletes the book with the given ID"""
+    try:
+        del books[id]
+        return "Book deleted successfully"
+    except KeyError:
+        raise HTTPException(status_code = 404, detail = "Book not found")
+
+@router.post("/{id}/review")
+def post_book_review(
+    id: Annotated[int, Path(description = "The ID of the book to review")],
+    review: Review
+):
+    """Adds a review to the book with the given ID"""
+    try:
+        books[id].review = review.review
+        return "Review added successfully"
+    except KeyError:
+        raise HTTPException(status_code = 404, detail = "Book not found")
